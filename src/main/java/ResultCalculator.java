0c8f90e41201a14060e8978d54ea5513febd2acc
szymonjasniak <ma1969MA>
2017-06-10 12:41:38 +0200

    Added evaluation of model

--- src/main/java/ResultCalculator.java
+++ src/main/java/ResultCalculator.java
@@ -73,6 +73,7 @@ public class ResultCalculator {
             String[] bestParameters = parameterSelection.getBestClassifierOptions();
             classifier.setOptions(bestParameters);
             classifier.buildClassifier(learningData);
+            eval.evaluateModel(classifier,eval.getHeader());
             return new SingleRunResult(eval.rootMeanSquaredError());
         }
     }
